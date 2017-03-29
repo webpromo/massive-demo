2c2dafda9ff130248f65fd867bc096cdc293c393
Jason Staten <jason.staten@domo.com>
2017-03-29 13:33:43 -0600

    dm20

---
+++ db/get_all_incidents.sql
@@ -0,0 +1,5 @@
+select inc.id, inc.us_state, inj.name as injury, aff.name as affectedArea, c.name as cause
+from incidents inc
+join injuries inj on inc.injury_id = inj.id
+join affected_areas aff on aff.id = inj.affected_area_id
+join causes c on c.id = inc.cause_id
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var massive = require('massive');
+
+var db = massive.connectSync({
+  connectionString : 'postgres://postgres:postgres@localhost/massive_demo'
+});
+
+console.log(db)
 
 var app = express();
 app.use(bodyParser.json());
@@ -7,11 +14,24 @@ app.use(bodyParser.json());
 var port = 3000;
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  db.get_all_injuries(function(err, injuries) {
+    res.send(injuries);
+  });
 });
 
 app.get('/incidents', function(req, res) {
-  res.send({incidents: []});
+  var state = req.query.us_state;
+
+  if (state) {
+    db.get_incidents_by_state([state], function(err, incidents) {
+      res.send(incidents);
+    });
+  }
+  else {
+    db.get_all_incidents(function(err, incidents) {
+      res.send(incidents);
+    });
+  }
 });
 
 app.post('/incidents', function(req, res) {

256f3151616546573acfb8978102057acd22e700
Jesse <rank_booster@yahoo.com>
2018-07-12 10:58:13 -0600

    initial commit

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const massive = require('massive');
+
+// test to see if Massive works
+const connectionString = "postgres://?ssl=true"
 
 const app = express();
 app.use(bodyParser.json());
 
 const port = 3000;
 
+app.set('cool','jason'); /// the two names don't matter, just must use elsewhere  // creates a value you can use elsewhere.
+
 app.get('/', (req, res) => {
-  res.send('massive-demo');
+  const db = req.app.get('db');  
+  db.getAllInjuries().then(injuries => {  // adds a method to db that we can call later
+    res.send(injuries);  
+  })   
+
 });
 
 app.get('/incidents', (req, res) => {
@@ -18,6 +28,9 @@ app.post('/incidents', (req, res) => {
   res.send({id: 123});
 });
 
-app.listen(port, () => {
-  console.log('Started server on port', port);
-});
+massive(connectionString).then(connection =>{
+  app.set('db',connection);  // our db connection is the 'db' setting 
+  app.listen(port, () => {
+    console.log('Started server on port', port);
+  });
+})
